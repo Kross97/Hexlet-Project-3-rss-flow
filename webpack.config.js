002db031b034b1913cf1901a624fcbc7dc02efd2
Nikita Bezhenar <kross78080@yandex.ru>
2019-09-17 00:32:48 +0300

    222

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+module.exports = {
+  mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, ' dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html',
+    }),
+  ],
+};
